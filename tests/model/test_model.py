95e195ae087928f32e61a1a8c8f8fc68d527896d
kbermude <kmbmha@hotmail.com>
2023-10-26 10:53:28 -0500

    fix bug in test_model_predict

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -28,7 +28,7 @@ class TestModel(unittest.TestCase):
     def setUp(self) -> None:
         super().setUp()
         self.model = DelayModel()
-        self.data = pd.read_csv(filepath_or_buffer="../data/data.csv")
+        self.data = pd.read_csv(filepath_or_buffer="data/data.csv")
         
 
     def test_model_preprocess_for_training(
@@ -90,10 +90,15 @@ class TestModel(unittest.TestCase):
     def test_model_predict(
         self
     ):
-        features = self.model.preprocess(
-            data=self.data
+        #Model should be fitted
+        features, target = self.model.preprocess(
+            data=self.data,
+            target_column="delay"
         )
-
+        self.model.fit(
+                    features=features,
+                    target=target
+                )
         predicted_targets = self.model.predict(
             features=features
         )

e7efbeedd9f86eb0321a0de2ba04d25542ba47fe
kbermude <kmbmha@hotmail.com>
2023-10-27 19:06:28 -0500

    Part 2 implement the api with fastapi

--- challenge/api.py
+++ challenge/api.py
@@ -1,7 +1,21 @@
-import fastapi
+from fastapi import FastAPI, HTTPException
+from challenge.model import DelayModel
+import pandas as pd
+from pydantic import BaseModel
+import numpy as np
 
-app = fastapi.FastAPI()
-
+app = FastAPI()
+model = DelayModel()
+data = pd.read_csv(filepath_or_buffer="data/data.csv")
+features, target = model.preprocess(
+            data=data,
+            target_column="delay"
+        )
+model.fit(
+    features=features,
+    target=target
+)
+OPERAS = list(set(data.OPERA))
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
     return {
@@ -9,5 +23,14 @@ async def get_health() -> dict:
     }
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(data: dict) -> dict:
+    
+    data = pd.DataFrame(data["flights"],dtype={"OPERA":str,"TIPOVUELO":str,"MES":int})
+    if  not data['MES'].between(1, 12,inclusive=True).all():
+        raise HTTPException(status_code=404, detail="Incorrect MES.")
+    if  np.isin(data["TIPOVUELO"],["N","I"]).all():
+        raise HTTPException(status_code=404, detail="Incorrect TIPOVUELO.")
+    if  np.isin(data["OPERA"],OPERAS).all():
+        raise HTTPException(status_code=404, detail="Incorrect OPERA.")
+    results = model.predict(data)    
+    return {"predict": results}
